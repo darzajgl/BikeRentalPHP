ac380faf7ff70d4de45bc7863071fa77ac7709b8
Dariusz Zajglic <darzajgl@gmail.com>
2022-12-30 13:28:51 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+SESSION_START();
+?>
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <title>Strona główna</title>
+    <link rel="stylesheet" href="style.css">
+    <link href='https://fonts.googleapis.com/css?family=Pacifico&subset=latin,latin-ext' rel='stylesheet'
+          type="text/css">
+</head>
+<body>
+<div id="container">
+    <!--    <div id="logo">-->
+    <!--        BIKE RENTAL-->
+    <!--    </div>-->
+
+
+    <div id="topbar">
+        <div id="topbar-left">
+            <img src="images/cyclist_ws.png">
+        </div>
+        <div id="topbar-right">
+            BIKE RENTAL<br/>
+        </div>
+        <div style="clear:both;"></div>
+    </div>
+
+    <div id="menu">
+        <div class="option">Strona Główna</div>
+        <div class="option">Konto</div>
+        <div class="option">Administracja</div>
+        <div class="option">Informacje</div>
+        <div class="option">Koszyk</div>
+        <div style="clear:both;"></div>
+    </div>
+<!--    <div id="sidebar">-->
+<!--        <div class="option-left">Strona Główna</div>-->
+<!--        <div class="option-left">Konto</div>-->
+<!--        <div class="option-left">Administracja</div>-->
+<!--        <div class="option-left">Koszyk</div>-->
+<!--        <div class="option-left">Informacje</div>-->
+<!--        <div style="clear:both;"></div>-->
+<!--    </div>-->
+
+
+    <div id="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dapibus mi ac augue sagittis, at euismod metus
+        dapibus. Suspendisse et diam sed urna luctus iaculis et ac nibh. Sed semper ex ut diam pellentesque rutrum.
+        Mauris et lorem eros. Nunc dictum imperdiet tellus, in viverra justo ultricies at. Quisque sagittis consequat
+        rutrum. Donec euismod odio sed nisl, sit amet porta urna luctus et. Sed sit amet nisl at diam eleifend facilisis
+        a
+        eget ligula. Praesent dolor velit, rhoncus in sodales sit amet, dapibus at velit. Pellentesque vulputate purus
+        nisi, ac viverra velit iaculis a.
+
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dapibus mi ac augue sagittis, at euismod metus
+        dapibus. Suspendisse et diam sed urna luctus iaculis et ac nibh. Sed semper ex ut diam pellentesque rutrum.
+        Mauris et lorem eros. Nunc dictum imperdiet tellus, in viverra justo ultricies at. Quisque sagittis consequat
+        rutrum. Donec euismod odio sed nisl ullamcorper, in tristique lectus tempor. Nam in est at nisi cursus convallis
+        vitae nec risus. Nam posuere velit vestibulum, placerat sapien vel, molestie
+        orci. Sed vel metus nec elit luctus venenatis id suscipit ipsum. Phasellus finibus dolor in laoreet ornare.
+        Suspendisse lobortis nisl nisl, sit amet porta urna luctus et. Sed sit amet nisl at diam eleifend facilisis a
+        eget ligula. Praesent dolor velit, rhoncus in sodales sit amet, dapibus at velit. Pellentesque vulputate purus
+        nisi, ac viverra velit iaculis a.
+    </div>
+    <div style="clear:both;"></div>
+    <div id="footer"> *********PROJEKT TI********</div>
+
+</div>
+
+</body>
+</html>
